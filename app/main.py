b34f3a467cb93b360c2820f78b625949f0e158bd
phamthanhdat270198 <phamthanhdat270198@gmail.com>
2025-03-30 23:52:52 +0700

    change port for public

--- app/main.py
+++ app/main.py
@@ -55,6 +55,6 @@ def admin_page():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8500)
     # uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 # 
